ab61060eb67281fa789eee107b586e1e3dd15b8d
Andrés Vargas <120044405+AndresVargasFT@users.noreply.github.com>
2023-05-27 14:59:20 -0600

    jwt test

--- src/modules/administration/users/users.controller.ts
+++ src/modules/administration/users/users.controller.ts
@@ -25,7 +25,6 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  @UseGuards(AuthGuard('jwt'))
   @ApiResponse({
     status: 200,
     description: 'Return the user logged with his roles and applications',
