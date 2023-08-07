026782196d8cad63a221fd42529c81be9224d208
Andrés Vargas <120044405+AndresVargasFT@users.noreply.github.com>
2023-08-06 21:46:54 -0600

    dto changes

--- src/modules/financial_control/payment/dto/create-payment.dto.ts
+++ src/modules/financial_control/payment/dto/create-payment.dto.ts
@@ -2,29 +2,93 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreatePaymentDto {
-  @ApiProperty()
   @IsNumber()
+  @ApiProperty()
+  @IsNotEmpty()
   employee_id: number;
 
-  @ApiProperty()
   @IsNumber()
-  extra_time: number;
+  @IsOptional()
+  biweekly_salary: number;
+
+  @IsNumber()
+  @IsOptional()
+  daily_salary: number;
+
+  @IsNumber()
+  @IsOptional()
+  subsidy: number;
+
+  @IsNumber()
+  @IsOptional()
+  hour_rate: number;
+
+  @IsOptional()
+  @IsNumber()
+  extra_time_value: number;
 
   @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  extra_time: number;
+
+  @IsOptional()
+  @IsNumber()
+  extra_time_total: number;
+
+  @IsOptional()
   @IsNumber()
   medical_leave_days: number;
 
   @ApiProperty()
+  @IsNotEmpty()
   @IsNumber()
   not_payed_leave_days: number;
 
-  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  gross_payment: number;
+
+  @IsOptional()
+  @IsNumber()
+  gross_payment_dollar: number;
+
+  @IsOptional()
+  @IsNumber()
+  gross_payment_social_deduction: number;
+
+  @IsOptional()
   @IsNumber()
   payment_advance: number;
 
+  @IsOptional()
+  @IsNumber()
+  deduction_total: number;
+
+  @IsOptional()
+  @IsNumber()
+  net_payment: number;
+
+  @IsOptional()
+  @IsNumber()
+  net_payment_dollar: number;
+
+  @IsOptional()
+  @IsNumber()
+  income_tax: number;
+
   @ApiProperty()
   @IsNumber()
+  @IsNotEmpty()
   dollar: number;
+
+  @IsOptional()
+  @IsNumber()
+  total_salary: number;
+
+  @IsOptional()
+  @IsNumber()
+  ins_payroll: number;
 
   @IsOptional()
   @IsString()
